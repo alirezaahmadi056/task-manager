43eb4765c52336697e1fb536bc10d0c998a2e588
Hosein Ahmadi <iamohoseinam@gmail.com>
2024-06-06 11:34:52 +0330

    change status bsr colors

--- app/src/main/java/ir/hoseinahmadi/taskmanager/ui/theme/Theme.kt
+++ app/src/main/java/ir/hoseinahmadi/taskmanager/ui/theme/Theme.kt
@@ -60,8 +60,8 @@ fun TaskManagerTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
