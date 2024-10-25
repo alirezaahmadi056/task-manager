c30ed65b020bc96bcfa535e5b9f5ae22ae5d5bd1
Hosein Ahmadi <iamohoseinam@gmail.com>
2024-10-25 18:21:56 +0330

    create notification channel

--- app/src/main/java/info/alirezaahmadi/taskmanager/App.kt
+++ app/src/main/java/info/alirezaahmadi/taskmanager/App.kt
@@ -7,25 +7,23 @@ import android.content.Context
 import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
 import info.alirezaahmadi.taskmanager.util.Constants.CHANNEL_ID
+import info.alirezaahmadi.taskmanager.util.Constants.CHANNEL_NAME
 
 @HiltAndroidApp
-class App : Application(){
+class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        createNotificationChannel()
-    }
-
-    private fun createNotificationChannel() {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "getString(R.string.channel_name)" // Channel name for the user
-            val descriptionText ="notif" // Channel description for the user
-            val importance = NotificationManager.IMPORTANCE_HIGH
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-                description = descriptionText
-            }
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            )
             notificationManager.createNotificationChannel(channel)
         }
     }
+
+
 }
