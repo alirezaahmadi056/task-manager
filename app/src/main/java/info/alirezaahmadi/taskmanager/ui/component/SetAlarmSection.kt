fa14345f88121f7de981bfe26a04b68ecc94af22
Hosein Ahmadi <iamohoseinam@gmail.com>
2024-10-26 13:25:26 +0330

    update SetAlarmSection

--- app/src/main/java/info/alirezaahmadi/taskmanager/ui/component/SetAlarmSection.kt
+++ app/src/main/java/info/alirezaahmadi/taskmanager/ui/component/SetAlarmSection.kt
@@ -1,18 +1,23 @@
 package info.alirezaahmadi.taskmanager.ui.component
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AlarmOff
 import androidx.compose.material.icons.rounded.AlarmOn
@@ -26,22 +31,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun SetAlarmSection(
+    enableAlarm:Boolean,
+    onEnable:(Boolean)->Unit,
     onSelectedDate: () -> Unit,
     onSelectedTime: () -> Unit,
-    times: List<String> = emptyList()
+    times: String,
+    dates: String,
 ) {
-    var showSection by rememberSaveable { mutableStateOf(false) }
+    var showSection by remember(key1 = enableAlarm) { mutableStateOf(enableAlarm) }
     Column(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth()
+            .animateContentSize(),
     ) {
         HorizontalDivider(
             thickness = 6.dp,
@@ -50,7 +61,7 @@ fun SetAlarmSection(
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 10.dp, vertical = 8.dp),
+                .padding(10.dp),
             horizontalArrangement = Arrangement.Absolute.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
@@ -58,13 +69,15 @@ fun SetAlarmSection(
                 checked = showSection,
                 onCheckedChange = {
                     showSection = it
+                    onEnable(it)
                 },
                 colors = SwitchDefaults.colors(
-                    checkedIconColor = MaterialTheme.colorScheme.primary.copy(0.8f),
-                    checkedThumbColor = MaterialTheme.colorScheme.background,
-                    uncheckedIconColor = Color.DarkGray,
-                    uncheckedBorderColor = Color.DarkGray.copy(0.8f),
-                    uncheckedThumbColor = Color.DarkGray,
+                    checkedIconColor = MaterialTheme.colorScheme.primary.copy(),
+                    checkedThumbColor = Color.White,
+                    uncheckedIconColor = MaterialTheme.colorScheme.scrim.copy(0.8f),
+                    uncheckedBorderColor = MaterialTheme.colorScheme.scrim.copy(0.8f),
+                    uncheckedThumbColor = MaterialTheme.colorScheme.scrim,
+                    uncheckedTrackColor = Color.White,
                 )
             )
             Row(
@@ -87,62 +100,99 @@ fun SetAlarmSection(
         AnimatedVisibility(
             visible = showSection,
             enter = fadeIn() + expandVertically(animationSpec = tween(800)),
-            exit = fadeOut() + shrinkVertically(animationSpec = tween(500))
+            exit = fadeOut() + shrinkVertically(animationSpec = tween(800))
         ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 10.dp, vertical = 5.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
+            Column(
+                modifier = Modifier.fillMaxWidth(),
             ) {
-                Text(
-                    text = "${times[0]}:${times[1]}"
-                )
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        imageVector = Icons.Rounded.DateRange,
-                        contentDescription = "",
-                        tint = MaterialTheme.colorScheme.scrim.copy(0.8f)
-                    )
-                    Spacer(Modifier.width(4.dp))
-                    Text(
-                        text = "تنظیم تاریخ",
-                        style = MaterialTheme.typography.bodyLarge,
-                        color = MaterialTheme.colorScheme.scrim.copy(0.8f)
-                    )
-                }
-
                 Row(
                     modifier = Modifier
+                        .clickable(onClick =onSelectedDate)
                         .fillMaxWidth()
-                        .padding(horizontal = 10.dp, vertical = 5.dp),
+                        .padding(15.dp),
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.SpaceBetween
-                ) { }
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Icon(
-                        imageVector = if (showSection) Icons.Rounded.AlarmOn else Icons.Rounded.AlarmOff,
-                        contentDescription = "",
-                        tint = if (!showSection) MaterialTheme.colorScheme.scrim.copy(0.8f) else MaterialTheme.colorScheme.primary
-                    )
-                    Spacer(Modifier.width(4.dp))
-                    Text(
-                        text = "تنظیم ساعت",
-                        style = MaterialTheme.typography.bodyLarge,
-                        color = if (!showSection) MaterialTheme.colorScheme.scrim.copy(0.8f) else MaterialTheme.colorScheme.primary
-                    )
-                }
 
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = Icons.Rounded.DateRange,
+                            contentDescription = "",
+                            tint = MaterialTheme.colorScheme.scrim.copy(0.8f)
+                        )
+                        Spacer(Modifier.width(6.dp))
+                        Text(
+                            text = "تاریخ",
+                            style = MaterialTheme.typography.bodyLarge,
+                            color = MaterialTheme.colorScheme.scrim.copy(0.8f)
+                        )
+                    }
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(6.dp))
+                            .border(
+                                .5.dp,
+                                color = MaterialTheme.colorScheme.scrim,
+                                shape = RoundedCornerShape(6.dp)
+                            ),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = dates,
+                            style = MaterialTheme.typography.titleMedium,
+                            color = MaterialTheme.colorScheme.scrim,
+                            modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+                        )
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .clickable(onClick = onSelectedTime)
+                        .fillMaxWidth()
+                        .padding(15.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = if (showSection) Icons.Rounded.AlarmOn else Icons.Rounded.AlarmOff,
+                            contentDescription = "",
+                            tint = MaterialTheme.colorScheme.scrim.copy(0.8f)
+                        )
+                        Spacer(Modifier.width(4.dp))
+                        Text(
+                            text = "ساعت",
+                            style = MaterialTheme.typography.bodyLarge,
+                            color = MaterialTheme.colorScheme.scrim.copy(0.8f)
+                        )
+                    }
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(6.dp))
+                            .border(
+                                .5.dp,
+                                color = MaterialTheme.colorScheme.scrim,
+                                shape = RoundedCornerShape(6.dp)
+                            ),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = times,
+                            style = MaterialTheme.typography.titleMedium,
+                            color = MaterialTheme.colorScheme.scrim,
+                            modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+                        )
+                    }
+                }
             }
-            HorizontalDivider(
-                thickness = 6.dp,
-                color = Color.LightGray.copy(0.1f)
-            )
         }
+        HorizontalDivider(
+            thickness = 6.dp,
+            color = Color.LightGray.copy(0.1f)
+        )
     }
 }
