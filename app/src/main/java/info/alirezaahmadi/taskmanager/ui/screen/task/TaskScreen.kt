f2b9436d73f2e6b8cf602583f4bc062567c5814c
Hosein Ahmadi <iamohoseinam@gmail.com>
2024-10-30 12:34:08 +0330

    update TaskScreen

--- app/src/main/java/info/alirezaahmadi/taskmanager/ui/screen/task/TaskScreen.kt
+++ app/src/main/java/info/alirezaahmadi/taskmanager/ui/screen/task/TaskScreen.kt
@@ -1,10 +1,23 @@
 package info.alirezaahmadi.taskmanager.ui.screen.task
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -13,15 +26,15 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.NoteAdd
 import androidx.compose.material.icons.rounded.DeleteSweep
 import androidx.compose.material.icons.rounded.EditNote
+import androidx.compose.material.icons.rounded.KeyboardArrowDown
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SwipeToDismissBox
 import androidx.compose.material3.SwipeToDismissBoxValue
@@ -30,17 +43,18 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLayoutDirection
@@ -52,16 +66,20 @@ import info.alirezaahmadi.taskmanager.data.db.task.TaskItem
 import info.alirezaahmadi.taskmanager.navigation.Screen
 import info.alirezaahmadi.taskmanager.ui.component.DialogDeleteItemTask
 import info.alirezaahmadi.taskmanager.ui.component.EmptyList
+import info.alirezaahmadi.taskmanager.ui.component.MySnackbarHost
 import info.alirezaahmadi.taskmanager.ui.component.SelectedSortNotList
 import info.alirezaahmadi.taskmanager.util.Constants
+import info.alirezaahmadi.taskmanager.util.TaskHelper
+import info.alirezaahmadi.taskmanager.viewModel.AlarmViewModel
 import info.alirezaahmadi.taskmanager.viewModel.TaskViewModel
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun TaskScreen(
     navHostController: NavHostController,
-    taskViewModel: TaskViewModel = hiltViewModel()
+    taskViewModel: TaskViewModel = hiltViewModel(),
+    alarmViewModel: AlarmViewModel = hiltViewModel()
 ) {
 
     val item by taskViewModel.allItem.collectAsState(initial = emptyList())
@@ -85,69 +103,74 @@ fun TaskScreen(
     )
 
 
-    var singleDeleteTask by remember {
-        mutableStateOf(TaskItem())
-    }
+    var singleDeleteTask by remember { mutableStateOf(TaskItem()) }
     var showDialogDelete by remember {
         mutableStateOf(false)
     }
     val snackBarHostState = remember { SnackbarHostState() }
     val scope = rememberCoroutineScope()
-
+    val context = LocalContext.current
     DialogDeleteItemTask(
-        title ="حذف وظیفه",
+        title = "حذف وظیفه",
         body = "آیا از حذف این وظیفه اطمینان دارید؟",
         onBack = { showDialogDelete = false },
         onDeleteItem = {
             taskViewModel.deleteTask(singleDeleteTask)
+              alarmViewModel.canselNotificationAlarm(
+                  context = context,
+                  id = singleDeleteTask.id
+              )
             showDialogDelete = false
             scope.launch {
                 snackBarHostState.showSnackbar(
                     "وظیفه با موفقیت حذف شد",
-                    duration = SnackbarDuration.Short
+                    duration = SnackbarDuration.Short,
+                    withDismissAction = true
                 )
             }
         },
         show = showDialogDelete
     )
 
-    var extanded by remember {
+    var expanded by remember {
         mutableStateOf(false)
     }
     val lazyListState = rememberLazyListState()
-
+    var expandedList by rememberSaveable {
+        mutableStateOf(false)
+    }
+    val rotateState by animateFloatAsState(
+        targetValue = if (expandedList) 180f else 0f,
+        label = ""
+    )
     Scaffold(
         snackbarHost = {
-            SnackbarHost(snackBarHostState) { data ->
-                Snackbar(
-                    action = {
-                        TextButton(onClick = {
-                            taskViewModel.upsertTask(singleDeleteTask)
-                            data.dismiss()
-                        }) {
-                            Text(
-                                "بازگردانی",
-                                color = MaterialTheme.colorScheme.primary,
-                                style = MaterialTheme.typography.bodyMedium
-                            )
-                        }
-                    },
-                    containerColor = MaterialTheme.colorScheme.secondary,
-                    shape = RoundedCornerShape(10.dp),
-                    modifier = Modifier.padding(vertical = 8.dp, horizontal = 4.dp)
-                ) {
-                    Text(
-                        data.visuals.message,
-                        color = MaterialTheme.colorScheme.background,
-                        style = MaterialTheme.typography.bodyMedium
-                    )
+            MySnackbarHost(
+                snackBarHostState,
+                action = {data->
+                    TextButton(onClick = {
+                        taskViewModel.upsertTask(singleDeleteTask)
+                        alarmViewModel.setNotificationAlarm(
+                            context = context,
+                            id = singleDeleteTask.id,
+                            title = singleDeleteTask.title,
+                            triggerTime = singleDeleteTask.triggerAlarmTime
+                        )
+                        data.dismiss()
+                    }) {
+                        Text(
+                            "بازگردانی",
+                            color = MaterialTheme.colorScheme.background,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
                 }
-            }
+            )
         },
         floatingActionButton = {
             ExtendedFloatingActionButton(
                 containerColor = MaterialTheme.colorScheme.primary,
-                expanded = extanded,
+                expanded = expanded,
                 text = {
                     Text(
                         text = "وظیفه",
@@ -162,23 +185,23 @@ fun TaskScreen(
                         tint = Color.White
                     )
                 },
-                onClick = { navHostController.navigate(Screen.AddTaskScreen.route) })
+                onClick = {
+                    val lastId = if (item.isNotEmpty()) item.last().id else 0
+                    navHostController.navigate(Screen.AddTaskScreen.route + "?lastId=${lastId}")
+                }
+            )
         },
         floatingActionButtonPosition = FabPosition.Start
     ) {
-        extanded =
+        expanded =
             (lazyListState.firstVisibleItemScrollOffset == 0 || lazyListState.canScrollForward)
         LazyColumn(
             state = lazyListState,
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
                 .padding(it),
         ) {
-            if (item.isEmpty()){
-                item {
-                    EmptyList()
-                }
-            }else{
+            if (item.isNotEmpty()) {
                 items(incompleteTasks, key = { task -> task.id }) { taskItem ->
                     CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
                         val swipeToDismiss = rememberSwipeToDismissBoxState(
@@ -187,6 +210,7 @@ fun TaskScreen(
                                     SwipeToDismissBoxValue.StartToEnd -> {
                                         navHostController.navigate(Screen.AddTaskScreen.route + "?id=${taskItem.id}")
                                     }
+
                                     SwipeToDismissBoxValue.EndToStart -> {
                                         singleDeleteTask = taskItem
                                         showDialogDelete = true
@@ -254,19 +278,141 @@ fun TaskScreen(
                         }
                     }
                 }
-                item {
-                    CompletedTaskSection(navHostController, completedTasks) { deleted ->
-                        singleDeleteTask = deleted
-                        showDialogDelete = true
+                stickyHeader {
+                    Row(
+                        modifier = Modifier
+                            .background(MaterialTheme.colorScheme.background)
+                            .fillMaxWidth()
+                            .padding(horizontal = 12.dp, vertical = 8.dp),
+                        horizontalArrangement = Arrangement.Start,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+
+                        Row(
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = " تکمیل شده",
+                                color = MaterialTheme.colorScheme.primary,
+
+                                style = MaterialTheme.typography.bodyLarge,
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(
+                                text = "( ${TaskHelper.taskByLocate(completedTasks.size.toString())} ${"وظیفه )"}",
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = MaterialTheme.colorScheme.scrim,
+                            )
+
+                        }
+
+
+                        IconButton(onClick = { expandedList = !expandedList }) {
+                            Icon(
+                                imageVector = Icons.Rounded.KeyboardArrowDown, contentDescription = "",
+                                modifier = Modifier
+                                    .rotate(rotateState),
+                                tint = MaterialTheme.colorScheme.scrim.copy(0.8f)
+                            )
+                        }
                     }
                 }
+                items(completedTasks, key = { task -> task.id }){ taskItem ->
+                    AnimatedVisibility(
+                        visible = expandedList,
+                        enter = fadeIn() + expandVertically(animationSpec = tween(1000)),
+                        exit = fadeOut() + shrinkVertically(animationSpec = tween(1000))
+                    ) {
+                        CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+                            val swipeToDismiss = rememberSwipeToDismissBoxState(
+                                confirmValueChange = { swip ->
+                                    when (swip) {
+                                        SwipeToDismissBoxValue.StartToEnd -> {
+                                            navHostController.navigate(Screen.AddTaskScreen.route + "?id=${taskItem.id}")
+                                        }
+
+                                        SwipeToDismissBoxValue.EndToStart -> {
+                                            singleDeleteTask = taskItem
+                                            showDialogDelete = true
+                                        }
+
+                                        SwipeToDismissBoxValue.Settled -> {
+                                        }
+                                    }
+                                    return@rememberSwipeToDismissBoxState false
+                                }
+                            )
+                            SwipeToDismissBox(
+                                enableDismissFromEndToStart = true,
+                                enableDismissFromStartToEnd = true,
+                                state = swipeToDismiss,
+                                backgroundContent = {
+                                    when (swipeToDismiss.dismissDirection) {
+                                        SwipeToDismissBoxValue.StartToEnd -> {
+                                            Box(
+                                                modifier = Modifier
+                                                    .fillMaxSize()
+                                                    .padding(5.dp)
+                                                    .clip(RoundedCornerShape(11.dp))
+                                                    .background(MaterialTheme.colorScheme.onPrimary),
+                                                contentAlignment = Alignment.CenterStart
+                                            )
+                                            {
+                                                Icon(
+                                                    Icons.Rounded.EditNote,
+                                                    contentDescription = "",
+                                                    tint = Color.White,
+                                                    modifier = Modifier.size(50.dp)
+                                                )
+
+                                            }
+                                        }
+
+                                        SwipeToDismissBoxValue.EndToStart -> {
+                                            Box(
+                                                modifier = Modifier
+                                                    .fillMaxSize()
+                                                    .padding(5.dp)
+                                                    .clip(RoundedCornerShape(11.dp))
+                                                    .background(MaterialTheme.colorScheme.error),
+                                                contentAlignment = Alignment.CenterEnd
+                                            )
+                                            {
+                                                Icon(
+                                                    Icons.Rounded.DeleteSweep,
+                                                    contentDescription = "",
+                                                    tint = Color.White, modifier = Modifier.size(50.dp)
+                                                )
+                                            }
+                                        }
+
+                                        SwipeToDismissBoxValue.Settled -> {}
+                                    }
+
+                                }
+                            ) {
+                                CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+                                    TaskItemCard(navHostController = navHostController, item = taskItem)
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                item { EmptyList() }
             }
 
         }
 
     }
 
-
 }
+
+
+
+
+
+
 
 
