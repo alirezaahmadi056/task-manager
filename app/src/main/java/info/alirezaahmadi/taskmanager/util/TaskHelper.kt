0a78f6bd1e74a6cc0995be6174312e81e9721078
Hosein Ahmadi <iamohoseinam@gmail.com>
2024-10-30 12:34:45 +0330

    add helper function in TaskHelper

--- app/src/main/java/info/alirezaahmadi/taskmanager/util/TaskHelper.kt
+++ app/src/main/java/info/alirezaahmadi/taskmanager/util/TaskHelper.kt
@@ -69,6 +69,26 @@ object TaskHelper {
 
         return calendar
     }
+    fun getTimeInMillis(calendar: Calendar,year: Int, month: Int, day: Int, hour: Int, minute: Int): Long {
+        // ایجاد یک شیء Calendar
+
+        // تنظیم سال، ماه، روز
+        calendar.set(Calendar.YEAR, year)
+        calendar.set(Calendar.MONTH, month - 1) // ماه‌ها از 0 شروع می‌شوند
+        calendar.set(Calendar.DAY_OF_MONTH, day)
+
+        // تنظیم ساعت، دقیقه، ثانیه
+        calendar.set(Calendar.HOUR_OF_DAY, hour)
+        calendar.set(Calendar.MINUTE, minute)
+        calendar.set(Calendar.SECOND, 0)
+
+        // تنظیم میلی‌ثانیه صفر برای دقت بیشتر
+        calendar.set(Calendar.MILLISECOND, 0)
+
+        // بازگرداندن زمان به صورت میلی‌ثانیه
+        return calendar.timeInMillis
+    }
+
     fun splitWholeDate(dateString: String): List<Int> {
 
         if (dateString == "") {
@@ -81,37 +101,6 @@ object TaskHelper {
         val day = parts[2].toInt()
 
         return listOf(year, month, day)
-    }
-
-    fun gregorianToJalali(gy: Int, gm: Int, gd: Int): String {
-
-        if (gy == 0 || gm == 0 || gd == 0) {
-            return ""
-        }
-
-        val gDaysInMonth: IntArray =
-            intArrayOf(0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334)
-        val gy2: Int = if (gm > 2) (gy + 1) else gy
-        var gTotalDays: Int =
-            355666 + (365 * gy) + ((gy2 + 3) / 4) - ((gy2 + 99) / 100) + ((gy2 + 399) / 400) + gd + gDaysInMonth[gm - 1]
-        var jy: Int = -1595 + (33 * (gTotalDays / 12053))
-        gTotalDays %= 12053
-        jy += 4 * (gTotalDays / 1461)
-        gTotalDays %= 1461
-        if (gTotalDays > 365) {
-            jy += ((gTotalDays - 1) / 365)
-            gTotalDays = (gTotalDays - 1) % 365
-        }
-        val jm: Int
-        val jd: Int
-        if (gTotalDays < 186) {
-            jm = 1 + (gTotalDays / 31)
-            jd = 1 + (gTotalDays % 31)
-        } else {
-            jm = 7 + ((gTotalDays - 186) / 30)
-            jd = 1 + ((gTotalDays - 186) % 30)
-        }
-        return "$jy-$jm-$jd"
     }
 
     fun jalaliToGregorian(jy: Int, jm: Int, jd: Int): String {
@@ -156,6 +145,60 @@ object TaskHelper {
         while (gm < 13 && gd > sal_a[gm]) gd -= sal_a[gm++]
         return "$gy-$gm-$gd"
     }
+    fun gregorianToJalali(gy: Int, gm: Int, gd: Int): String {
 
+        if (gy == 0 || gm == 0 || gd == 0) {
+            return "انتخاب نشده"
+        }
+
+        val gDaysInMonth: IntArray =
+            intArrayOf(0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334)
+        val gy2: Int = if (gm > 2) (gy + 1) else gy
+        var gTotalDays: Int =
+            355666 + (365 * gy) + ((gy2 + 3) / 4) - ((gy2 + 99) / 100) + ((gy2 + 399) / 400) + gd + gDaysInMonth[gm - 1]
+        var jy: Int = -1595 + (33 * (gTotalDays / 12053))
+        gTotalDays %= 12053
+        jy += 4 * (gTotalDays / 1461)
+        gTotalDays %= 1461
+        if (gTotalDays > 365) {
+            jy += ((gTotalDays - 1) / 365)
+            gTotalDays = (gTotalDays - 1) % 365
+        }
+        val jm: Int
+        val jd: Int
+        if (gTotalDays < 186) {
+            jm = 1 + (gTotalDays / 31)
+            jd = 1 + (gTotalDays % 31)
+        } else {
+            jm = 7 + ((gTotalDays - 186) / 30)
+            jd = 1 + ((gTotalDays - 186) % 30)
+        }
+        return "$jy-$jm-$jd"
+    }
+
+    // تابعی برای تبدیل میلی‌ثانیه به تاریخ میلادی به صورت لیست [سال، ماه، روز]
+    fun convertMillisToDateList(millis: Long): List<Int> {
+        val calendar = Calendar.getInstance().apply {
+            timeInMillis = millis
+        }
+
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH) + 1 // افزودن 1 چون ماه‌ها از 0 شروع می‌شوند
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        return listOf(year, month, day)
+    }
+
+    // تابعی برای تبدیل میلی‌ثانیه به زمان به صورت لیست [ساعت، دقیقه، ثانیه]
+    fun convertMillisToTimeList(millis: Long): List<Int> {
+        val calendar = Calendar.getInstance().apply {
+            timeInMillis = millis
+        }
+
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+
+        return listOf(hour,minute)
+    }
 }
 
