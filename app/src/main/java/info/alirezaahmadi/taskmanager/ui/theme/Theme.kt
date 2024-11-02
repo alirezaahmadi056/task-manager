eb57b50801482bd0926cc074b98b62092284844f
Hosein Ahmadi <iamohoseinam@gmail.com>
2024-11-02 09:42:15 +0330

    fix navigationBarColors

--- app/src/main/java/info/alirezaahmadi/taskmanager/ui/theme/Theme.kt
+++ app/src/main/java/info/alirezaahmadi/taskmanager/ui/theme/Theme.kt
@@ -56,6 +56,7 @@ fun TaskManagerTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.background.toArgb()
+            window.navigationBarColor =colorScheme.background.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
