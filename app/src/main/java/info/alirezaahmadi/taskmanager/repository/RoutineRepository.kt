e3bf91bb686db0338488ee60d296002285b16aec
Hosein Ahmadi <iamohoseinam@gmail.com>
2024-11-13 11:05:03 +0330

    create routine repo and viewModel

---
+++ app/src/main/java/info/alirezaahmadi/taskmanager/repository/RoutineRepository.kt
@@ -0,0 +1,24 @@
+package info.alirezaahmadi.taskmanager.repository
+
+import androidx.room.Query
+import androidx.room.Upsert
+import info.alirezaahmadi.taskmanager.data.db.routine.RoutineDao
+import info.alirezaahmadi.taskmanager.data.db.routine.RoutineItem
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class RoutineRepository @Inject constructor(
+    private val dao: RoutineDao
+) {
+
+    suspend fun upsertRoutine(item: RoutineItem) {
+        dao.upsertRoutine(item)
+    }
+
+    suspend fun deletedById(itemId: Int) {
+        dao.deletedById(itemId)
+    }
+
+    fun getAllRoutine(): Flow<List<RoutineItem>> = dao.getAllRoutine()
+
+}
