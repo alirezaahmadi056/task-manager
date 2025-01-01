7163946008455664e1115f57d3dd145d6a10658c
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-01-01 14:06:00 +0330

    fix label text

--- app/src/main/java/info/alirezaahmadi/taskmanager/ui/graph/skinRoutine/SkinRoutineDialog.kt
+++ app/src/main/java/info/alirezaahmadi/taskmanager/ui/graph/skinRoutine/SkinRoutineDialog.kt
@@ -1,10 +1,7 @@
 package info.alirezaahmadi.taskmanager.ui.graph.skinRoutine
 
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Canvas
+
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,9 +9,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
@@ -24,8 +19,6 @@ import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
