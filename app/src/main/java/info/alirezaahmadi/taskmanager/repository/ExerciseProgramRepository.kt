c082cff646a923f4c7721cb189caa15d81a3dc37
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-01-17 11:26:56 +0330

    update ExerciseProgramViewModel and repo

--- app/src/main/java/info/alirezaahmadi/taskmanager/repository/ExerciseProgramRepository.kt
+++ app/src/main/java/info/alirezaahmadi/taskmanager/repository/ExerciseProgramRepository.kt
@@ -1,10 +1,19 @@
 package info.alirezaahmadi.taskmanager.repository
 
 import info.alirezaahmadi.taskmanager.data.db.exerciseProgram.ExerciseProgramDao
+import info.alirezaahmadi.taskmanager.data.db.exerciseProgram.ExerciseProgramItem
+import info.alirezaahmadi.taskmanager.data.db.skinRoutine.SkinRoutineItem
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ExerciseProgramRepository @Inject constructor(
     private val exerciseProgramDao: ExerciseProgramDao
 ){
+
+    suspend fun upsertExercise(item: ExerciseProgramItem) { exerciseProgramDao.upsertExerciseProgram(item) }
+
+    suspend fun deletedById(itemId: Int) { exerciseProgramDao.deletedExerciseProgram(itemId) }
+    fun getAllExercise(): Flow<List<ExerciseProgramItem>> = exerciseProgramDao.getAllExerciseProgram()
+    fun getSkinRoutine(itemId: Int): Flow<ExerciseProgramItem?> = exerciseProgramDao.getExerciseProgram(itemId)
 
 }
--- app/src/main/java/info/alirezaahmadi/taskmanager/viewModel/ExerciseProgramViewModel.kt
+++ app/src/main/java/info/alirezaahmadi/taskmanager/viewModel/ExerciseProgramViewModel.kt
@@ -1,13 +1,24 @@
 package info.alirezaahmadi.taskmanager.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import info.alirezaahmadi.taskmanager.data.db.exerciseProgram.ExerciseProgramItem
 import info.alirezaahmadi.taskmanager.repository.ExerciseProgramRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ExerciseProgramViewModel @Inject constructor(
     private val repository: ExerciseProgramRepository
-):ViewModel() {
+) : ViewModel() {
+    fun upsertExerciseProgram(exerciseItem: ExerciseProgramItem) {
+        viewModelScope.launch(Dispatchers.IO) { repository.upsertExercise(exerciseItem) }
+    }
+    fun getAllExerciseProgram(): Flow<List<ExerciseProgramItem>> = repository.getAllExercise()
+    fun deletedExerciseProgram(id: Int) { viewModelScope.launch { repository.deletedById(id) } }
 
+    fun getExerciseProgram(itemId: Int): Flow<ExerciseProgramItem?> = repository.getSkinRoutine(itemId)
 }
