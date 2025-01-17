7a96686c921876d90307a184c9f4902778983b0e
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-01-17 23:22:01 +0330

    update ExerciseProgramViewModel

--- app/src/main/java/info/alirezaahmadi/taskmanager/viewModel/ExerciseProgramViewModel.kt
+++ app/src/main/java/info/alirezaahmadi/taskmanager/viewModel/ExerciseProgramViewModel.kt
@@ -7,6 +7,10 @@ import info.alirezaahmadi.taskmanager.data.db.exerciseProgram.ExerciseProgramIte
 import info.alirezaahmadi.taskmanager.repository.ExerciseProgramRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,11 +18,26 @@ import javax.inject.Inject
 class ExerciseProgramViewModel @Inject constructor(
     private val repository: ExerciseProgramRepository
 ) : ViewModel() {
+    private val _allExerciseProgram = MutableStateFlow<List<ExerciseProgramItem>>(emptyList())
+    val allExerciseProgram: StateFlow<List<ExerciseProgramItem>> = _allExerciseProgram.asStateFlow()
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.getAllExercise().collectLatest {
+                _allExerciseProgram.emit(it)
+            }
+        }
+    }
+
     fun upsertExerciseProgram(exerciseItem: ExerciseProgramItem) {
         viewModelScope.launch(Dispatchers.IO) { repository.upsertExercise(exerciseItem) }
     }
-    fun getAllExerciseProgram(): Flow<List<ExerciseProgramItem>> = repository.getAllExercise()
-    fun deletedExerciseProgram(id: Int) { viewModelScope.launch { repository.deletedById(id) } }
 
-    fun getExerciseProgram(itemId: Int): Flow<ExerciseProgramItem?> = repository.getSkinRoutine(itemId)
+
+    fun deletedExerciseProgram(id: Int) {
+        viewModelScope.launch { repository.deletedById(id) }
+    }
+
+    fun getExerciseProgram(itemId: Int): Flow<ExerciseProgramItem?> =
+        repository.getSkinRoutine(itemId)
 }
