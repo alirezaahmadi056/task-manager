12409b23d1b79e24eedead010c6e8affe75bf0d7
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-01-17 23:20:48 +0330

    create BaseImageLoader

---
+++ app/src/main/java/info/alirezaahmadi/taskmanager/ui/component/BaseImageLoader.kt
@@ -0,0 +1,25 @@
+package info.alirezaahmadi.taskmanager.ui.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import info.alirezaahmadi.taskmanager.R
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun BaseImageLoader(
+    modifier: Modifier = Modifier,
+    model:Any,
+    contentScale: ContentScale
+) {
+    GlideImage(
+        model = model,
+        modifier = modifier,
+        contentDescription = "",
+        contentScale = contentScale
+    ){
+        it.placeholder(R.drawable.ic_launcher_background)
+    }
+}
