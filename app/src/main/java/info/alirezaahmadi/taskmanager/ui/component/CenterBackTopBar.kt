4e4026eb2969c0d647fe49bc53e1135cf294c967
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-01-25 12:43:13 +0330

    add CenterBackTopBar

---
+++ app/src/main/java/info/alirezaahmadi/taskmanager/ui/component/CenterBackTopBar.kt
@@ -0,0 +1,52 @@
+package info.alirezaahmadi.taskmanager.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import info.alirezaahmadi.taskmanager.R
+
+@Composable
+fun CenterBackTopBar(
+    text :String,
+    onBack: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .background(Color.White)
+            .statusBarsPadding()
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 16.dp),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyLarge.copy(fontSize = 19.sp),
+            color = Color.Black,
+            modifier = Modifier.align(Alignment.Center),
+            fontWeight = FontWeight.Black
+        )
+        IconButton(onClick = onBack) {
+            Icon(
+                imageVector = Icons.Rounded.ArrowForward,
+                contentDescription = "",
+                tint = Color.Black
+            )
+        }
+    }
+}
