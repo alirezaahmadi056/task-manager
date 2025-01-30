c8567b1055b85e7cab205633fe348f914d4dd81f
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-01-30 20:31:07 +0330

    update FirstScreen

--- app/src/main/java/info/alirezaahmadi/taskmanager/ui/graph/first/FirstScreen.kt
+++ app/src/main/java/info/alirezaahmadi/taskmanager/ui/graph/first/FirstScreen.kt
@@ -3,8 +3,10 @@ package info.alirezaahmadi.taskmanager.ui.graph.first
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
@@ -17,11 +19,17 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.QuestionMark
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
@@ -29,6 +37,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -68,51 +79,129 @@ fun FirstScreen(
             )
         },
         content = {
-            FlowRow(
-                modifier = Modifier.fillMaxSize(),
-                maxLines = 5,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                SelectedGraphRoute(
-                    text = "وظایف",
-                ) { navHostController.navigate(Screen.DutiesGraph) }
+            Scaffold(
+                containerColor = MaterialTheme.colorScheme.background,
+                topBar = {
+                    FistTopBar(
+                        openMenu = { scope.launch { drawerState.open() } },
+                        openHelp = {}
+                    )
+                }
+            ) { innerPadding ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                ) {
 
-                SelectedGraphRoute(
-                    text = "روتیم",
-                ) { navHostController.navigate(Screen.WeeklyRoutineGraph) }
-                SelectedGraphRoute(
-                    text = "پوستی",
-                ) { navHostController.navigate(Screen.SkinRoutineGraph) }
+                    FlowRow(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        maxItemsInEachRow = 2,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        SelectedGraphRoute(
+                            text = "مدیریت وظایف ",
+                            image = R.drawable.duties_image
+                        ) { navHostController.navigate(Screen.DutiesGraph) }
 
-                SelectedGraphRoute(
-                    text = "باشگاه",
-                ) { navHostController.navigate(Screen.ExerciseProgramGraph) }
+                        SelectedGraphRoute(
+                            text = "روتین هفتگی",
+                            image = R.drawable.weekly_routine_image
+                        ) { navHostController.navigate(Screen.WeeklyRoutineGraph) }
+                        SelectedGraphRoute(
+                            text = "روتین پوستی",
+                            image = R.drawable.skin_routine_image
+                        ) { navHostController.navigate(Screen.SkinRoutineGraph) }
 
-                SelectedGraphRoute(
-                    text = "اهداف",
-                ) { navHostController.navigate(Screen.GoalsGraph) }
+                        SelectedGraphRoute(
+                            text = "برنامه باشگاه",
+                            image = R.drawable.ecericies_image
+                        ) { navHostController.navigate(Screen.ExerciseProgramGraph) }
 
-                SelectedGraphRoute(
-                    text = "دارو",
-                ) { navHostController.navigate(Screen.MedicineGraph) }
-                SelectedGraphRoute(
-                    text = "رویا",
-                ) { navHostController.navigate(Screen.DreamGraph) }
+                        SelectedGraphRoute(
+                            text = "اهداف من",
+                            image = R.drawable.goals_image
+                        ) { navHostController.navigate(Screen.GoalsGraph) }
+
+                        SelectedGraphRoute(
+                            text = "برنامه دارویی",
+                            image = R.drawable.medicine_image
+                        ) { navHostController.navigate(Screen.MedicineGraph) }
+                        SelectedGraphRoute(
+                            text = "رویاهای من",
+                            image = R.drawable.dream_image
+                        ) { navHostController.navigate(Screen.DreamGraph) }
+
+                    }
+                }
 
             }
+
         })
+}
+
+@Composable
+private fun FistTopBar(
+    openMenu: () -> Unit,
+    openHelp: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .drawBehind {
+                drawLine(
+                    color = Color.LightGray.copy(alpha = 0.5f),
+                    start = Offset(0f, size.height), // شروع خط در پایین کامپوزبل
+                    end = Offset(size.width, size.height), // پایان خط در پایین کامپوزبل
+                    strokeWidth = 1.5.dp.toPx()
+                )
+            }
+            .padding(9.dp)
+    ) {
+        IconButton(
+            modifier = Modifier.align(Alignment.CenterStart),
+            onClick = openMenu
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.Menu,
+                contentDescription = "",
+                tint = MaterialTheme.colorScheme.onBackground
+            )
+        }
+        Image(
+            painter = painterResource(R.drawable.first_top_logo),
+            contentScale = ContentScale.Crop,
+            contentDescription = "",
+            modifier = Modifier
+                .size(40.dp)
+                .align(Alignment.Center),
+        )
+        IconButton(
+            modifier = Modifier.align(Alignment.CenterEnd),
+            onClick = openHelp
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.message_question),
+                contentDescription = "",
+                tint = MaterialTheme.colorScheme.onBackground
+            )
+        }
+    }
 }
 
 @Composable
 fun SelectedGraphRoute(
     text: String,
-    @DrawableRes image: Int = R.drawable.lesson,
+    @DrawableRes image: Int,
     onClick: () -> Unit
 ) {
     Column(
         modifier = Modifier
-            .padding(8.dp)
+            .fillMaxWidth(0.5f)
+            .padding(6.dp)
             .clip(RoundedCornerShape(12.dp))
+            .background(MaterialTheme.colorScheme.onPrimaryContainer)
             .clickable(onClick = onClick)
             .padding(12.dp),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -121,7 +210,7 @@ fun SelectedGraphRoute(
             painter = painterResource(image),
             contentDescription = "",
             modifier = Modifier
-                .size(80.dp),
+                .size(75.dp),
             contentScale = ContentScale.Crop
         )
         Spacer(Modifier.height(6.dp))
@@ -132,6 +221,7 @@ fun SelectedGraphRoute(
             color = MaterialTheme.colorScheme.scrim,
             textAlign = TextAlign.Center
         )
+        Spacer(Modifier.height(6.dp))
     }
 
 }
